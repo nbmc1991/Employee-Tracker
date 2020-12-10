e63f911d61be6b166ae62d85d207248960ac3702
nbmc1991 <nbmc1991@gmail.com>
2020-12-10 14:10:08 -0500

    going modular

---
+++ db/db.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+// var ct = require("console.table");
+
+
+//setting up connection information for the sqp database
+var connection = mysql.createConnection({
+    host: "localhost",
+    //port
+    port: 3306,
+
+    //username
+    user: "root",
+
+    //passworkd
+    password: "Lucas2018.",
+    database: "employee_db"
+});
+//connect to mysql server and sql database
+connection.connect(err => {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+    //call function to start
+});
+
+
+module.exports.connection = connection
